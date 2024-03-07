8ee3946608fff974d00fd06d7d7b1bf8cdf7aee6
Marcos Leite <m.abner@live.co.uk>
2024-03-07 02:08:10 -0300

    adds the self parameters in the methods and instantiates the PhotoSort object

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from PIL import Image
 class PhotoSort:
     extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff', '.psd', '.cr2', '.dng', '.raw']
 
-    def folder_path(file):
-        date = photo_shooting_date(file)
+    def folder_path(self, file):
+        date = self.photo_shooting_date(file)
         return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
 
-    def photo_shooting_date(file):
+    def photo_shooting_date(self, file):
         img = Image.open(file)
         info = img.getexif()
         if 36867 in info:
@@ -22,8 +22,8 @@ class PhotoSort:
             date = datetime.fromtimestamp(os.path.getmtime(file))
         return date
 
-    def move_file(file):
-        new_folder = folder_path(file)
+    def move_file(self, file):
+        new_folder = self.folder_path(file)
         if not os.path.exists(new_folder):
             os.makedirs(new_folder)
         shutil.move(file, new_folder + '/' + file)
@@ -34,4 +34,5 @@ class PhotoSort:
             self.move_file(filename)
 
 
-print(organize_files())
+PS = PhotoSort()
+PS.organize_files()
