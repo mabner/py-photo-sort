4ba5fae304615ac4e1340096f7cc70bded3330ef
Marcos Leite <m.abner@live.co.uk>
2024-03-07 02:39:10 -0300

    refactors the photo_shooting_date function

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ class PhotoSort:
         return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
 
     def photo_shooting_date(self, file):
+        date = None
         img = Image.open(file)
-        info = img.getexif()
-        if self.DATETIME_EXIF_INFO_ID in info:
-            date = info[self.DATETIME_EXIF_INFO_ID]
-            date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
-        else:
+        if hasattr(img, '_getexif'):
+            info = img._getexif()
+            if info:
+                if self.DATETIME_EXIF_INFO_ID in info:
+                    date = info[self.DATETIME_EXIF_INFO_ID]
+                    date = datetime.strptime(date, '%Y:%m:%d %H:%M:%S')
+        if date is None:
             date = datetime.fromtimestamp(os.path.getmtime(file))
         return date
 
@@ -32,9 +35,8 @@ class PhotoSort:
 
     def organize_files(self):
         photo = [filename for filename in os.listdir('.')
-                 if os.path.isfile(filename) and
-                 any(filename.lower().endswith('.' + ext.lower())
-                     for ext in self.extensions)]
+                 if os.path.isfile(filename) and any(filename.lower().endswith('.' + ext.lower())
+                                                     for ext in self.extensions)]
         for filename in photo:
             self.move_file(filename)
 
